8b59c8b0e40d8353cb2b7105e505f1485336b663
kevin ventura <kevcoder@kevins-mbp.myfiosgateway.com>
2022-11-09 00:25:57 -0500

    function added that allows button to reveal question when clicked

--- js/app.js
+++ js/app.js
@@ -1,2 +1,25 @@
 //using selectors inside the element
 // traversing the dom
+const questions = document.querySelectorAll('.question');
+
+questions.forEach(function(question){
+  console.log(question);
+  const btn = question.querySelector('.question-btn');
+  console.log(btn);
+  btn.addEventListener('click', function(){
+    question.classList.toggle('show-text');
+  })
+})
+
+
+
+//Traversing the dom
+// const btns = document.querySelectorAll('.question-btn');
+
+// btns.forEach(function(btn){
+//   btn.addEventListener('click', function(e){
+//     const question = e.currentTarget.parentElement.parentElement;
+    
+//     question.classList.toggle('show-text');
+//   });
+// });
